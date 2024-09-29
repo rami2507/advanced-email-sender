9b44f2492f9dfceba317e0f439c02fd0ab7d5085
Rami Laloui <lalouirami34@gmail.com>
2024-09-29 15:53:09 +0100

    Updated configs

--- transporter.js
+++ transporter.js
@@ -1,16 +1,15 @@
 const nodemailer = require("nodemailer");
 
 exports.transport = nodemailer.createTransport({
-  host: "maravendas.fun",
-  port: 587, // Correct port for SMTPS (SSL/TLS)
-  secure: false, // Use SMTPS (SSL/TLS)
+  host: "SMTP HOST",
+  port: "SMTP PORT",
+  secure: false,
   auth: {
-    user: "backing@maravendas.fun",
-    pass: "u8Uq/?_jx%4^9",
+    user: "SMTP USERNAME",
+    pass: "SMTP PASSWORD",
   },
   tls: {
-    // These options are usually not needed for SMTPS (SSL/TLS)
     minVersion: "TLSv1.2",
-    rejectUnauthorized: false, // This can be set to false in some cases
+    rejectUnauthorized: false,
   },
 });
